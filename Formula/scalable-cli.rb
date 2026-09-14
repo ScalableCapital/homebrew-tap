1a9e9b229deda3ed38e70a348db163fec4d3a3a6
Florian Benz <florian.benz@scalable.capital>
2026-09-14 10:25:50 +0200

    fix: declare scalable-cli formula as macOS-only

--- Formula/scalable-cli.rb
+++ Formula/scalable-cli.rb
@@ -1,25 +1,20 @@
 class ScalableCli < Formula
   desc "Scalable CLI"
   homepage "https://github.com/ScalableCapital/scalable-cli"
+  depends_on :macos
 
-  on_macos do
-    url "https://github.com/ScalableCapital/scalable-cli/releases/download/v1.0.0/sc-v1.0.0-macos-universal2.zip"
-    version "1.0.0"
-    sha256 "9f307672a08a8270b0deb9354e55d77b3c612d0f73e4c2ec9cb4b6b52bdccbf5"
+  url "https://github.com/ScalableCapital/scalable-cli/releases/download/v1.0.0/sc-v1.0.0-macos-universal2.zip"
+  version "1.0.0"
+  sha256 "9f307672a08a8270b0deb9354e55d77b3c612d0f73e4c2ec9cb4b6b52bdccbf5"
 
-    def install
-      (prefix/"Sc.app").mkpath
-      (prefix/"Sc.app").install Dir["Contents"]
-      bin.write_exec_script(prefix/"Sc.app/Contents/MacOS/sc")
-    end
-
-    test do
-      output = shell_output("#{bin}/sc --version")
-      assert_match version.to_s, output
-    end
+  def install
+    (prefix/"Sc.app").mkpath
+    (prefix/"Sc.app").install Dir["Contents"]
+    bin.write_exec_script(prefix/"Sc.app/Contents/MacOS/sc")
   end
 
-  on_linux do
-    odie "The scalable-cli formula is only available on macOS."
+  test do
+    output = shell_output("#{bin}/sc --version")
+    assert_match version.to_s, output
   end
 end
